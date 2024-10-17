1f9450728de15d25668f0a08cfee45a1450fd3e4
leo-seft <leosefton13@hotmail.com>
2024-10-17 12:55:17 +0100

    adding solution

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-// Write your code here
+const lightbulb1 = document.querySelector("#lightbulb1");
+const lightbulb2 = document.querySelector("#lightbulb2");
+const lightbulb3 = document.querySelector("#lightbulb3");
+const lightbulbs = [lightbulb1,lightbulb2,lightbulb3];
+
+const counter = document.querySelector(".subtitle");
+let count = 0;
+for (let i = 0; i < lightbulbs.length; i++){
+lightbulbs[i].addEventListener("click", function(event){
+    count++;
+    counter.innerHTML = "You've clicked the lights " + count + " times"
+    lightbulbs[i].classList.toggle("active")
+})
+}
